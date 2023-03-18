eeb0657981d88bb3d5fafa73a76e162e587d9a44
Mohammed M <mohamgoham@gmail.com>
2023-03-18 01:44:07 -0400

    complete client

--- client/src/main.rs
+++ client/src/main.rs
@@ -33,4 +33,19 @@ fn main() -> Result<(), anyhow::Error> {
         )
         arg_required_else_help(true)
         .get_matches();
+
+    let api_client = api::Client::new(config::SERVER_URL.to_string());
+
+    if let Some(_) = cli.subcommand_matches(cli::AGENTS) {
+        cli::agents::run(&api_client)?;
+    } else if let Some(_) = cli.subcommand_matches(cli::JOBS) {
+        cli::jobs::run(&api_client)?;
+    } else if let Some(matches) = cli.subcommand_matches(cli::EXEC) {
+        // safely unwrap cuz the args are a req
+        let agent_id = matches.value_of("agent_id").unwrap();
+        let command = matches.value_of("command").unwrap();
+        cli::exec::run(&api_client, agent_id, command)?;
+    }
+
+    Ok(())
 }
