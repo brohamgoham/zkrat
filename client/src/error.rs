ef5b9a0baf8cec0365af9704e2050619592821c6
Mohammed M <mohamgoham@gmail.com>
2023-03-19 12:53:56 -0400

    added docker file + make

--- client/src/error.rs
+++ client/src/error.rs
@@ -19,3 +19,21 @@ impl std::convert::From<reqwest::Error> for Error {
         Error::Internal(err.to_string())
     }
 }
+
+
+pub struct SlyCooper {
+    pub leo: String,
+    pub location: DateTime
+}
+
+impl SlyCooper {
+    pub fn set(&mut self, new_data: &str) {
+        self.leo = new_data.to_string();
+    }
+
+    pub fn rev() {
+        let s = String::new();
+        let rev = s.chars().rev().collect::<String>();
+        
+    }
+}
